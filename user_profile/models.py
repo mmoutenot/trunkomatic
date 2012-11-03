044dfdd8feaf763d49ce696b913ca8b596b4f987
Marshall <mmoutenot@gmail.com>
2012-11-03 17:32:35 -0400

    changing user_model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.contrib.localflavor.us.forms import USPhoneNumberField
+
 class UserProfile(models.Model):
   user = models.ForeignKey(User, related_name='profile')
 
-  phone_number = models.CharField(max_length=10)
+  phone_number = USPhoneNumberField()
 
   CARRIER_CHOICES = (('VZ','verizon'),
                      ('AT','att'),
