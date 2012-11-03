ee387867f640007c476621ed11927eae71d61169
Marshall <mmoutenot@gmail.com>
2012-11-03 02:13:35 -0400

    add apps to settings

--- trunkomatic/settings.py
+++ trunkomatic/settings.py
@@ -117,6 +117,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'user_profile',
+    'show',
 )
 
 # A sample logging configuration. The only tangible logging
