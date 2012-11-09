ee37ef6190471dcceeb0986ba22a1d135b113237
Marshall <mmoutenot@gmail.com>
2012-11-08 21:01:40 -0500

    fix settings for heroku use

--- trunkomatic/settings.py
+++ trunkomatic/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'trunkomatic',                      # Or path to database file if using sqlite3.
         'USER': 'trunkomatic',                      # Not used with sqlite3.
         'PASSWORD': 'trunkomatic',                  # Not used with sqlite3.
@@ -157,7 +157,5 @@ LOGGING = {
     }
 }
 
-AUTH_PROFILE_MODULE = "user_profile.userprofile"
-
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
